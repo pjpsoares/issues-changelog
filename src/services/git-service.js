a26833876f4c77f333d1a181684e0251c3a6e236
SoaresP <paulo.soares@blip.pt>
2015-10-18 22:39:25 +0100

    implemented fetching the commits messages

--- src/services/git-service.js
+++ src/services/git-service.js
@@ -1,20 +1,52 @@
 'use strict';
 
+var COMMITS_SEPARATOR = '<---/-/-/-/-/-/-/-/--->';
+var DEFAULT_FORMAT = '%B';
 var nodefn = require('when/node');
 var execAsPromise = nodefn.lift(require('child_process').exec);
 var semverValid = require('semver').valid;
+var template = require('lodash/string/template');
+var trim = require('lodash/string/trim');
 var getTagsCmd = 'git tag --sort version:refname';
+var getCommitsCmdTemplate =
+    template('git log --format="<%= format %>' + COMMITS_SEPARATOR + '"' +
+        ' "<%- fromTag ? fromTag + ".." : "" %><%= toTag %>"');
+
+function processExecResponse(data) {
+    return data[0];
+}
 
 function getTags() {
     return execAsPromise(getTagsCmd)
-        .then(function(data) {
-            return data[0].split('\n')
-                .filter(semverValid);
+        .then(processExecResponse)
+        .then(function splitTagsString(tagsString) {
+            return tagsString.split('\n');
+        })
+        .then(function filterInvalidTags(tags) {
+            return tags.filter(semverValid);
         });
 }
 
 function getCommits(fromTag, toTag) {
+    var getCommitsCmd = getCommitsCmdTemplate({
+        fromTag: fromTag,
+        toTag: toTag,
+        format: DEFAULT_FORMAT
+    });
 
+    return execAsPromise(getCommitsCmd)
+        .then(processExecResponse)
+        .then(function splitCommitsString(commitsString) {
+            return commitsString.split(COMMITS_SEPARATOR);
+        })
+        .then(function trimDownCommits(commits) {
+            return commits.map(trim);
+        })
+        .then(function cleanUpEmptyCommits(commits) {
+            return commits.filter(function unity(commit) {
+                return commit;
+            });
+        });
 }
 
 module.exports = {
