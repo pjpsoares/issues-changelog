3c42849182c62244c50efa45671b62e7e7d06da7
SoaresP <paulo.soares@blip.pt>
2015-10-19 20:08:01 +0100

    creating gitlab service to fetch the issue id

--- src/services/gitlab-service.js
+++ src/services/gitlab-service.js
@@ -1,2 +1,26 @@
 'use strict';
 
+var when = require('when');
+
+function createGitlabClient() {
+    var gitlab = require('gitlab')({
+        url:   'https://gitlab.app.betfair',
+        token: 'yrDp9zMRZniyhKiuG6C7'
+    });
+
+    function getIssue(projectId, issueId) {
+        var deferred = when.defer();
+
+        gitlab.projects.issues.list (projectId, {iid: issueId}, function (data) {
+            deferred.resolve(data && data[0]);
+        });
+
+        return deferred.promise;
+    }
+
+    return {
+        getIssue: getIssue
+    }
+}
+
+module.exports = createGitlabClient();
