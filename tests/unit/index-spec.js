f1487a565d1a5885bab65d7edb235d49aa60a1b3
Paulo Soares <paulo@yieldify.com>
2016-02-17 00:25:45 +0000

    Add script for unit tests

--- tests/unit/index-spec.js
+++ tests/unit/index-spec.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var chai = require('chai');
+var expect = chai.expect;
+
 describe('index', function () {
 
+    it('first test', function () {
+        expect(true).to.equal(true);
+    });
 });
